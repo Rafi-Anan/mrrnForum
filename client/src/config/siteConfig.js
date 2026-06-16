fe4b79b582974dfa424f291cbf221474a4f2321b
unknown <rafibstd@gmail.com>
2026-06-17 02:42:30 +0600

    Update forum features

--- client/src/config/siteConfig.js
+++ client/src/config/siteConfig.js
@@ -1,10 +1,12 @@
+const backendUrl = import.meta.env.VITE_API_URL?.replace(/\/api\/?$/, '') || "https://addin.h2r.online";
+
 const siteConfig = {
   siteName: "AD-Din Professional Forum",
   logo: "/Ad-Din.png",
   email: "rafibstd@gmail.com",
   phone: "+8801303025015",
   address: "Nabinagar, Brahmanbaria, Bangladesh",
-  backendUrl: "http://localhost:5000"
+  backendUrl
 };
 
 export default siteConfig;
